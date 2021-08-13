8f3dbd9c0e0159161f1459a350dfdac05e72a41d
Alexander Blair <ablair@jagtech.io>
2021-08-13 04:35:13 -0700

    Address-Add: Address Decoding Test
    Note: The address system does NOT currently perform a keccak256 check to verify that the checksum is valid.

--- support/address.go
+++ support/address.go
@@ -1,9 +1,17 @@
 package support
 
-func AddressDecode() bool {
-	return false
-}
+import (
+	"bytes"
+	"github.com/Snipa22/go-xmr-lib/support/Base58"
+)
 
-func AddressDecodeIntegrated() bool {
-	return false
+func ValidateAddress(address string, tag []byte) (bool, error) {
+	addr, err := Base58.DecodeFromString(address)
+	if err != nil {
+		return false, err
+	}
+	if bytes.Compare(addr[0:len(tag)], tag) != 0 {
+		return false, nil
+	}
+	return true, nil
 }
---
+++ support/address_test.go
@@ -0,0 +1,33 @@
+package support
+
+import "testing"
+
+func TestValidateAddress(t *testing.T) {
+	type args struct {
+		address string
+		tag     []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{"Valid Main Address", args{"491JTRtHaWdYLfgEkkkWgV6tmURjx8W2nRcZ1QUJ9mYSEmqzg5CDfTZCQquNcheYuwDphLbYw8HjYhes9dKtsVBS7aPb6LC", []byte{0x12}}, true, false},
+		{"Valid SubAddress", args{"87i7kA61fNvMboXiYWHVygPAggKJPETFqLXXcdH4mQTrECvrTxZMtt6e6owj1k8jUVjNR11eBuBMWHFBtxAwEVcm9dcSUxr", []byte{0x2A}}, true, false},
+		{"Valid Main Address, invalid tag", args{"491JTRtHaWdYLfgEkkkWgV6tmURjx8W2nRcZ1QUJ9mYSEmqzg5CDfTZCQquNcheYuwDphLbYw8HjYhes9dKtsVBS7aPb6LC", []byte{0x13}}, false, false},
+		{"Valid SubAddress, invalid tag", args{"87i7kA61fNvMboXiYWHVygPAggKJPETFqLXXcdH4mQTrECvrTxZMtt6e6owj1k8jUVjNR11eBuBMWHFBtxAwEVcm9dcSUxr", []byte{0x2B}}, false, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ValidateAddress(tt.args.address, tt.args.tag)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("AddressDecode() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("AddressDecode() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
